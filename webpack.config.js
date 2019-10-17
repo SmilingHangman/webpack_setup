6f588320438412af895c6aee204808c08a0b01a3
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-10-17 20:06:28 +0300

    webpack setup continued

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js'
@@ -17,5 +20,12 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HTMLWebpackPlugin(),
+        new CleanWebpackPlugin()
+    ]
 }
