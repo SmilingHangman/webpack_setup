a1181c8b4b0116e8d7e721002146de54bccaf440
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-10-21 17:56:36 +0300

    webpack setup continued

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './assets/style.css'
+import './assets/style.css';
+import image from './assets/bnb.png';
+const section = document.createElement('div');
+document.body.appendChild(section);
+
+const img = new Image();
+img.src = image;
+document.body.appendChild(img);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var { CleanWebpackPlugin } = require('clean-webpack-plugin');
 var path = require('path');
@@ -5,6 +6,7 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
+    devtool: 'inline-source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js'
@@ -12,12 +14,27 @@ module.exports = {
     module: {
         rules: [
             { 
-            test: /\.css$/,
-            use: 
+                test: /\.scss$/,
+                use:
                 [
-                'style-loader',
-                'css-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                          publicPath: (resourcePath, context) => {
+                            return path.relative(path.dirname(resourcePath), context) + '/';
+                            },
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                }
             }
         ]
     },
@@ -25,7 +42,14 @@ module.exports = {
         contentBase: './dist',
     },
     plugins: [
-        new HTMLWebpackPlugin(),
-        new CleanWebpackPlugin()
+        new HTMLWebpackPlugin({
+            title: 'Mano Title',
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ]
 }
